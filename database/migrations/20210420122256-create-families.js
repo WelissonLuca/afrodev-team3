262a0ab4bd6f8f576aed8d326223fb5a21f2e5a8
Welisson luca <welissonluca17@gmail.com>
2021-04-20 10:06:26 -0300

    feat: families migration add

---
+++ database/migrations/20210420122256-create-families.js
@@ -0,0 +1,56 @@
+module.exports = {
+  up: async (queryInterface, Sequelize) => queryInterface.createTable('families', {
+    id: {
+      type: Sequelize.INTEGER,
+      primaryKey: true,
+      allowNull: false,
+      autoIncrement: true,
+    },
+    name: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    age: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    cpf: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    address: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    civil_status: {
+      type: Sequelize.ENUM('Casado', 'Solteiro', 'Divorciado', 'Viuvo'),
+      allowNull: false,
+    },
+    gender: {
+      type: Sequelize.ENUM('Masculino', 'Feminino'),
+      allowNull: false,
+    },
+    number_members: {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+    },
+    children: {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+    },
+    per_capita_income: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    created_at: {
+      type: Sequelize.DATE,
+      allowNull: false,
+    },
+    updated_at: {
+      type: Sequelize.DATE,
+      allowNull: false,
+    },
+  }),
+
+  down: async (queryInterface) => queryInterface.dropTable('families'),
+};
