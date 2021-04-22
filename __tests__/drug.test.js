83d9df2a21980045149e017491223e5ab1cf8964
Lucas Oliveira <lucas996oliveira@gmail.com>
2021-04-22 16:12:41 -0300

    feature-create-tests-Drugs

---
+++ __tests__/drug.test.js
@@ -0,0 +1,45 @@
+const supertest = require('supertest')
+const app = require('../index')
+
+const request = supertest(app)
+const sequelize = require('../config/connection')
+
+const Drug =require('../application/model/drug')
+
+describe('Ensure to get data from API - DRUG', () => {
+
+  beforeAll(async () => {
+    await sequelize.authenticate()
+  })
+  
+
+
+  it('Ensure to GET Endpoint without parameters return content', async () => {
+    const res = await request.get('/drug')
+    expect(res.status).toBe(200)
+    expect(res.body).toStrictEqual([])
+  })
+
+
+  it('Ensure to POST endpoint insert content on database', async () => {
+   
+     const res = await request.post('/drug').send({
+      name: "Benegrip",
+      description: "Medicamento para gripe",
+      quantity: 18.1,
+      category: "Comum"
+     })
+     expect(res.status).toBe(200)
+     expect({
+      name: "Benegrip",
+      description: "Medicamento para gripe",
+      quantity: 18.1,
+      category: "Comum"
+     })
+    })
+
+  afterAll(async (done) => {
+    await sequelize.close()
+    done()
+  })
+})  
