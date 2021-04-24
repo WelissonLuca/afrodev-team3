935bd42d13b7029e19ad55ad3a2091299ed17c63
William Jesus <williamjesusti@gmail.com>
2021-04-23 21:55:37 -0300

    feature: criando os serviços de desapesas

--- config/connection.js
+++ config/connection.js
@@ -2,9 +2,11 @@ const { Sequelize } = require('sequelize');
 const dbConfiguration = require('./database.config');
 const Ong = require('../application/model/ong');
 const Supply = require('../application/model/supply');
+const Expense = require('../application/model/expense');
 
 const connection = new Sequelize(dbConfiguration);
 Ong.init(connection);
 Supply.init(connection);
+Expense.init(connection);
 
 module.exports = connection;
